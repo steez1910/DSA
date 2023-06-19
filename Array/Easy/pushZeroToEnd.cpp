11a48a7b59ebe18d35d85e9fde3f0d26960e3f34
steez1910 <a9638642@gmail.com>
2023-06-19 22:15:31 +0300

    3rd task

---
+++ Array/Easy/pushZeroToEnd.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pushZerosToEnd(int arr[], int n)
+{ 
+ int j = 0;
+ for (int i = 0; i < n; i++) 
+  {
+    if (arr[i] != 0) 
+      {
+        swap(arr[j], arr[i]); 
+        j++;
+      }
+  }
+ for (int i = 0; i < n; i++) 
+  {
+      cout << arr[i] << " ";
+  }
+  
+
+}
+
+
+int main()
+{
+  int arr[] = {12, 0, 32, 0, 0, 54, 4};
+  int n = sizeof(arr)/sizeof(arr[0]); 
+
+pushZerosToEnd(arr,n);
+
+return 0;
+}
--- Array/Easy/secondLagrgestelement.cpp
+++ Array/Easy/secondLagrgestelement.cpp
@@ -4,44 +4,36 @@ using namespace std;
 
 
 
-// void secondLargest(int arr[], int size)
-// {
-//   int first = INT_MIN;
-//   int second = INT_MIN;
-//   for (int i = 0; i < size; i++)
-//   {
-//     if (arr[i] > first)
-//     {
-//       second = first;
-//       first = arr[i] ;
-      
-//     }
-//     else if (arr[i] > second && arr[i] != first)
-//     {
-//       second = arr[i];
-      
-//     }
-   
-//   }
-//   cout << second <<endl;
-// }
-
-void sum(int arr[], int n)
+void secondLargest(int arr[], int size)
 {
-  int summ=0;
-    for (int i = 0; i < n; i++)
-     {
-      summ = summ + arr[i];
+  int first = INT_MIN;
+  int second = INT_MIN;
+  for (int i = 0; i < size; i++)
+  {
+    if (arr[i] > first)
+    {
+      second = first;
+      first = arr[i] ;
+      
     }
-    cout << summ;
+    else if (arr[i] > second && arr[i] != first)
+    {
+      second = arr[i];
+      
+    }
+   
+  }
+  cout << second <<endl;
 }
+
+
 
 int main()
 {
   int arr[] = {2, 1, 1, 4,};
   int n = sizeof(arr)/sizeof(arr[0]);
-  // secondLargest(arr, n);
-  sum(arr, n);
+  secondLargest(arr, n);
+
   return 0;
 
 }
